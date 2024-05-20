8b73ec96067b720807b8033573a723b9cc8674ed
andrewnyrov <andrewnyr@mail.ru>
2024-05-20 05:33:16 +0500

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,15 @@
+# Организуйте программу
+my_string = input()
+print(my_string)
+print(len(my_string))
+print(   )
+
+# Работа с методами строк
+task = 'Работа с методами строк'
+print(task)
+my_string = 'ЗДРАВСТВУЙ ДО СВИДАНИЯ'
+print(my_string.upper())
+print(my_string.lower())
+print((my_string).replace(' ', ''))
+print(my_string[:1])
+print(my_string[-1:])
